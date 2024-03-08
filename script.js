8d24e7275de6d6927abfb4f25c2fe84f2c9a41cd
nay60 <coulombeljeremy@outlook.fr>
2024-03-08 22:26:07 +0100

    nouveau code

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuHam = document.querySelector(".menu-ham")
+const navlink = document.querySelector(".nav-links")
+menuHam.addEventListener("click", ()=>{
+    navlink.classList.toggle('mobile-menu')
+    
+})
