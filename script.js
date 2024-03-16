a5c33afdf9377c0c28b0c6d1a5028f1203a72aff
nay60 <coulombeljeremy@outlook.fr>
2024-03-16 18:31:04 +0100

    push

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
+
 const menuHam = document.querySelector(".menu-ham")
 const navlink = document.querySelector(".nav-links")
 menuHam.addEventListener("click", ()=>{
     navlink.classList.toggle('mobile-menu')
     
 })
+
+const buton = document.querySelector(".submit")
+buton.addEventListener("click", function(){
+    const form = document.querySelector(".form")
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
+        const info = {
+            nom : event.target.querySelector("[name=nom]").value,
+            prenom : event.target.querySelector("[name=prenom]").value,
+            mail : event.target.querySelector("[name=mail]").value,
+        };
+        
+
+
+    });
+})
+    
