554f3258fce486d61c3fd1c962cc0547af364534
nay60 <coulombeljeremy@outlook.fr>
2024-03-20 20:33:27 +0100

    push

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+AOS.init(); 
 
 const menuHam = document.querySelector(".menu-ham")
 const navlink = document.querySelector(".nav-links")
@@ -9,8 +10,10 @@ menuHam.addEventListener("click", ()=>{
 const infopeople = []
 const buton = document.querySelector(".submit")
 
-    const form = document.querySelector(".form")
-    form.addEventListener("submit", function(event){
+const form = document.querySelector(".form")
+if (form){
+        form.addEventListener("submit", function(event){
+        
         event.preventDefault();
         const info = {
             nom : event.target.querySelector("[name=nom]").value,
@@ -18,8 +21,27 @@ const buton = document.querySelector(".submit")
             mail : event.target.querySelector("[name=mail]").value,
         };
         console.log(info);
-        infopeople.push(info);
+        infopeople.push(info);  
         console.log(infopeople);
         const donneinfo = JSON.stringify(infopeople);
         localStorage.setItem('donne', donneinfo);
-    });
+        event.target.querySelector("[name=nom]").value = "";
+        event.target.querySelector("[name=prenom]").value = "";
+        event.target.querySelector("[name=mail]").value = "";
+    })
+}
+
+
+const who = document.querySelector(".qui")
+
+window.addEventListener("scroll", ()=>{
+    const scrollpos = window.scrollY
+
+    if (scrollpos > 50){
+        who.classList.add('size')
+    }
+    else {
+        who.classList.remove('size')
+    }
+})
+
